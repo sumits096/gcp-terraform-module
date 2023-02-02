4cdab77f4e753fb3be1e0ae89f0d85993786451d
Sumit <sumits@incubxperts.com>
2023-02-02 18:36:49 +0530

    feat: added script to create cloud task permission

--- modules/cloud-task-permission/main.tf
+++ modules/cloud-task-permission/main.tf
@@ -3,5 +3,5 @@ resource "google_cloud_tasks_queue_iam_member" "cloud_task_member" {
   location = var.region
   name     = var.cloud_tasks_queue_name
   role     = "roles/cloudtasks.enqueuer"
-  member   = "serviceAccount:388793584861-compute@developer.gserviceaccount.com"
+  member   = "serviceAccount:boomtown-connector-sa@connectors-playground.iam.gserviceaccount.com"
 }
