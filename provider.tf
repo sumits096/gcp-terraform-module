1c89d85181eb3ab1742e0d238de2ea20171be828
Sumit <sumits@incubxperts.com>
2023-03-10 19:22:22 +0530

    feat: added azuread application script

--- provider.tf
+++ provider.tf
@@ -1,27 +1,27 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-    }
-    google-beta = {
-      source = "hashicorp/google-beta"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     google = {
+#       source = "hashicorp/google"
+#     }
+#     google-beta = {
+#       source = "hashicorp/google-beta"
+#     }
+#   }
+# }
 
-provider "google" {
-  credentials = var.service_account_credentials
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-}
+# provider "google" {
+#   credentials = var.service_account_credentials
+#   project     = var.project_id
+#   region      = var.region
+#   zone        = var.zone
+# }
 
-provider "google-beta" {
-  credentials = var.service_account_credentials
-  project     = var.project_id
-  region      = var.region
-  zone        = var.zone
-}
+# provider "google-beta" {
+#   credentials = var.service_account_credentials
+#   project     = var.project_id
+#   region      = var.region
+#   zone        = var.zone
+# }
 
 # provider "google" {
 #   alias        = "impersonated"
