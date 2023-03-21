547093504dc529b5299b98d8d9c2c68434efa4e0
Sumit <sumits@incubxperts.com>
2023-03-21 15:10:25 +0530

    refactor: resource deletion test

--- modules/azure-ad/application/outputs.tf
+++ modules/azure-ad/application/outputs.tf
@@ -1,16 +1,19 @@
-# output "application_id" {
-#   value = azuread_application.main.application_id
-# }
+output "application_id" {
+  count = 0
+  value = azuread_application.main[count.index].application_id
+}
 
-# output "client_secret" {
-#   description = "Client Secret"
-#   value       = azuread_application_password.secret.value
-# }
+output "client_secret" {
+  count       = 0
+  description = "Client Secret"
+  value       = azuread_application_password.secret[count.index].value
+}
 
-# output "display_name" {
-#   value = azuread_application.main.display_name
-# }
+output "display_name" {
+  count = 0
+  value = azuread_application.main[count.index].display_name
+}
 
-# output "redirect_uris" {
-#   value = var.azuread_app_redirect_uris
-# }
+output "redirect_uris" {
+  value = var.azuread_app_redirect_uris
+}
