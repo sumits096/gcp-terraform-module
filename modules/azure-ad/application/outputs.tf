7f36d67dd54bfbb06806a043b3e463464998df36
Sumit Shinde <92353125+sumits096@users.noreply.github.com>
2023-03-21 16:26:38 +0530

    Update outputs.tf

--- modules/azure-ad/application/outputs.tf
+++ modules/azure-ad/application/outputs.tf
@@ -1,16 +1,13 @@
 output "application_id" {
-  count = 0
   value = azuread_application.main[count.index].application_id
 }
 
 output "client_secret" {
-  count       = 0
   description = "Client Secret"
   value       = azuread_application_password.secret[count.index].value
 }
 
 output "display_name" {
-  count = 0
   value = azuread_application.main[count.index].display_name
 }
 
